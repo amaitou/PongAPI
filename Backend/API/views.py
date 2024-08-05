d07726bbaba079d8896eb75dfd2b35d3a9689338
amaitou <amait-ou@student.1337.ma>
2024-08-05 15:27:18 +0100

    working on user authentication

--- Backend/API/views.py
+++ Backend/API/views.py
@@ -12,49 +12,69 @@ def Register(request):
 		last_name = request.POST.get('last_name')
 		username = request.POST.get('username')
 		email = request.POST.get('email')
-		password = request.POST.get('password1')
+		password1 = request.POST.get('password1')
 		password2 = request.POST.get('password2')
 		date_joined = request.POST.get('date_joined')
 		gender = request.POST.get("gender")
-	
-		if password == password2:
+
+		if PlayerInfo.objects.filter(username=username).exists():
+			return JsonResponse({'status': 'failed', 'message': 'Username already exists!'})
+
+		else:
+			if password1 != password2:
+				return JsonResponse({'status': 'failed', 'message': 'Password does not match!'})
+
 			user = PlayerInfo.objects.create(username=username,
 							email=email,
-							password=make_password(password),
+							password=make_password(password1),
 							date_joined=date_joined,
 							first_name=first_name,
 							last_name=last_name,
 							gender=gender)
 			user.save()
-			return JsonResponse({'status': 'success', 'message': 'User created successfully!'})
-		else:
-			return JsonResponse({'status': 'failed', 'message': 'Password does not match!'})
+	return JsonResponse({'status': 'success', 'message': 'User created successfully!'})
 
 @csrf_exempt
 def Login(request):
 	
 	if request.method == 'POST':
 
+		username = request.POST.get('username')
+		if not PlayerInfo.objects.filter(username=username).exists():
+			return JsonResponse({'status': 'failed', 'message': 'User not found!'})
+
 		if request.user.is_authenticated:
 			return JsonResponse({'status': 'failed', 'message': 'User already logged in!'})
-		username = request.POST.get('username')
+
 		password = request.POST.get('password')
-		if check_password(password, PlayerInfo.objects.get(username=username).password):
-			user = authenticate(request, username=username, password=password)
-			if user is not None:
-				login(request, user)
-				return JsonResponse({'status': 'success', 'message': 'User logged in successfully!'})
-			else:
-				return JsonResponse({'status': 'failed', 'message': 'User not found!'})
-		else:
+		if not check_password(password, PlayerInfo.objects.get(username=username).password):
 			return JsonResponse({'status': 'failed', 'message': 'Incorrect password!'})
+		
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return JsonResponse({'status': 'success', 'message': 'User logged in successfully!'})
 
 @csrf_exempt
 def Logout(request):
 	
 	if request.method == 'POST':
-		if request.user.is_authenticated:
-			logout(request)
-			return JsonResponse({'status': 'success', 'message': 'User logged out successfully!'})
-		else:
+		if not request.user.is_authenticated:
 			return JsonResponse({'status': 'failed', 'message': 'User not logged in!'})
+
+		logout(request)
+		return JsonResponse({'status': 'success', 'message': 'User logged out successfully!'})			
+
+
+@csrf_exempt
+def Delete(request):
+
+	if request.method == 'POST':
+
+		username = request.POST.get('username')
+		if not PlayerInfo.objects.filter(username=username).exists():
+			return JsonResponse({'status': 'failed', 'message': 'User not found!'})
+
+		user = PlayerInfo.objects.get(username=username)
+		user.delete()
+		return JsonResponse({'status': 'success', 'message': 'User deleted successfully!'})
