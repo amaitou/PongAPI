8e2bc443914ac199ff980e9610a85af877fb7cfb
amaitou <amait-ou@student.1337.ma>
2024-08-09 20:23:30 +0100

    the token will rely on the if instead of the username (username could be changed)

--- Backend/API/authentication.py
+++ Backend/API/authentication.py
@@ -6,5 +6,5 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        token['username'] = user.username
+        token['id'] = user.id
         return token
