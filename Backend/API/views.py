2b71b62fa961622d1603e4b80c244a93721470a8
amaitou <amait-ou@student.1337.ma>
2024-08-10 23:31:09 +0100

    still working on JWT Authentication

--- Backend/API/views.py
+++ Backend/API/views.py
@@ -1,13 +1,13 @@
 
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import IsAuthenticated
 from .authentication import CookieTokenAuthentication
 from .serializers import PlayerInfoSerializer
-from rest_framework.response import Response
 from django.contrib.auth import authenticate
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from django.conf import settings
@@ -49,28 +49,21 @@ class LoginView(APIView):
             return Response({"error": "Invalid credentials"}, status=400)
 
 class GetPlayer(APIView):
-    class TokenExpired(Exception):
-        pass
+
+    authentication_classes = [CookieTokenAuthentication]
 
     def get(self, request):
         access_token = request.COOKIES.get(settings.ACCESS_TOKEN)
-        refresh_token = request.COOKIES.get(settings.REFRESH_TOKEN)
 
-        if not access_token or not refresh_token:
-            return Response({'error': 'Authentication credentials were not provided.'}, status=status.HTTP_401_UNAUTHORIZED)
-
+        if not access_token:
+            return Response({'error': 'No token provided.'},
+                            status=status.HTTP_400_BAD_REQUEST)
         try:
             access_token = AccessToken(access_token)
-        except InvalidToken:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+        except TokenError:
+            return Response({'error': 'Invalid token.'},
+                            status=InvalidToken.status_code)
         
-        try:
-            expiration = access_token['exp']
-            if expiration < time.time():
-                raise self.TokenExpired
-        except self.TokenExpired:
-            return Response({'error': 'Token was expired'}, status=status.HTTP_401_UNAUTHORIZED)
-
         player = PlayerInfo.objects.get(id=access_token['user_id'])
         serializer = PlayerInfoSerializer(player)
         return Response(serializer.data,
