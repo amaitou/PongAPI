d11fcb6a46e92e81d02346a068de58d7b2ba75d9
amaitou <amait-ou@student.1337.ma>
2024-08-29 12:34:13 +0100

    refactoring the code

--- Backend/API/urls.py
+++ Backend/API/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('callback', Authentication42View.as_view(), name='callback'),
+    path('register/', RegisterView.as_view(), name='registration'),
+    path('callback', Authentication42View.as_view(), name='42_authentication'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('refresh/', TokenRefresherView.as_view(), name='refresh'),
-    path('users/', AllUsersView.as_view(), name='token'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('profile/<str:username>/', ProfileView.as_view(), name='profile'),
+    path('refresh/', TokenRefresherView.as_view(), name='token_refresher'),
+    path('users/', AllUsersView.as_view(), name='all_users'),
+    path('profile/', ProfileView.as_view(), name='profile_owner'),
+    path('profile/<str:username>/', ProfileView.as_view(), name='user_profile'),
     path("password_u/", PasswordUpdateView.as_view(), name="password_update"),
     path("profile_u/", ProfileUpdateView.as_view(), name="profile_update"),
     path("email_v/", EmailVerifyView.as_view(), name="email_verification"),
