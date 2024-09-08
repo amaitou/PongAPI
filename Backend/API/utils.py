4e1246a6b9154e7e41065018b61462c3644bf259
amaitou <amait-ou@student.1337.ma>
2024-09-08 08:22:34 +0100

    __doc__ for Utils.py

--- Backend/API/utils.py
+++ Backend/API/utils.py
@@ -1,32 +1,46 @@
-
+from django.core.mail import EmailMessage
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from django.core.exceptions import ValidationError
-from django.core.mail import EmailMessage
 from .models import UserInfo
 
 class Utils:
+    """
+    This class contains utility methods used throughout the application.
+    """
 
     @staticmethod
     def send_verification_email(data):
+        """
+        This method sends a verification email to a user.
+        The email's subject, body, and recipient are specified in the 'data' dictionary.
+        """
         email = EmailMessage(subject=data['subject'], body=data['body'], to=[data['email']])
         email.send()
-    
+
     @staticmethod
     def create_jwt_for_user(user) -> dict:
+        """
+        This method creates a JWT (JSON Web Token) for a given user.
+        It returns a dictionary containing the access and refresh tokens.
+        """
         refresh = RefreshToken.for_user(user)
         access_token = str(refresh.access_token)
         refresh_token = str(refresh)
-        
+
         jwt = {
             'access_token': access_token,
             'refresh_token': refresh_token
         }
 
         return jwt
-    
+
     @staticmethod
     def get_user_from_jwt(token, __type) -> UserInfo:
-
+        """
+        This method retrieves a user from a given JWT.
+        The type of token ('access' or 'refresh') is specified by '__type'.
+        If the user does not exist or an error occurs, it returns None.
+        """
         if __type == 'refresh':
             try:
                 refresh_token = RefreshToken(token)
@@ -49,10 +63,13 @@ class Utils:
                     return None
             except Exception as e:
                 return None
-    
+
     @staticmethod
     def password_validation(password):
-
+        """
+        This method validates a password according to certain criteria.
+        If the password does not meet these criteria, it raises a ValidationError with a list of errors.
+        """
         errors = []
 
         if len(password) < 8:
@@ -65,6 +82,6 @@ class Utils:
             errors.append('Password must contain at least one lowercase letter')
         if not any(char in '!@#$%^&*()_+' for char in password):
             errors.append('Password must contain at least one special character')
-        
+
         if errors:
             raise ValidationError(errors)
